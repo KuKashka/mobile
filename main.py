7e868584a9242a6b08e31b58ac95af32d20dae43
KuKashka <ostovrah@gmail.com>
2024-09-26 20:30:31 +0300

    додали погю

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ from kivy.lang  import Builder
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
+import requests
+from settings import *
 
 
 class WeatherScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+    def get_weather(self, city):
+        params = {
+            "q": city,
+            "appid":API_KEY,
+        }
+        data = requests.get(CURRENT_WEATHER_URL,params)
+        response = data.json()
+        print(response)
+        return response
+    
+    def search(self):
+        city = self.ids.city.text
+        weather = self.get_weather(city)
+        
+
 
 
 class LCloudApp(MDApp):
@@ -16,6 +35,4 @@ class LCloudApp(MDApp):
         self.theme_cls.primary_palette = "Purple"
 
         return WeatherScreen()
-
-
 LCloudApp().run()
