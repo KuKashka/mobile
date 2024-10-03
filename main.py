be8381f7ff052f750a6235eaa2e777cd6c85951f
KuKashka <ostovrah@gmail.com>
2024-10-03 20:28:31 +0300

    змінив код

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRectangleFlatButton
 import requests
 from settings import *
-
+#Ну прівєтікі
 
 class WeatherScreen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -25,8 +25,27 @@ class WeatherScreen(MDScreen):
         city = self.ids.city.text
         weather = self.get_weather(city)
         
+        temp = weather["main"]["temp"]
+        self.ids.temp.text = f"{temp}°C"
+
+        feels_like = weather["main"]["feels_like"]
+        self.ids.feels_like.text = f"Відчуваєтся як {round(feels_like)}°C"
 
 
+        desc = weather["weather"][0]["description"]
+        self.ids.desc.text = desc.capitalize()
+
+
+        humidity = weather["main"]["humidity"]
+        self.ids.humidity.text = f"Вологість: {humidity}%"
+
+
+        wind = weather["wind"]["speed"]
+        self.ids.wind.text = f"Вітер: {wind} М/С"
+
+
+        icon = weather["weather"][0]["icon"]
+        self.ids.icon.source = f"https://openweathermap.org/img/wn/{icon}@2x.png"
 
 class LCloudApp(MDApp):
     def build(self):
